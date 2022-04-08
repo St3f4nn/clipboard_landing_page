4a0be0f0ec0c7b1fa49c2fa97696176de6300975
Stefan Gajić <stefan.gajic@nenasal.com>
2022-04-08 21:36:55 +0200

    Styling the header and his responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     './dist/*.html'
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+    extend: {
+      colors: {
+        fourd5760: '#4d5760',
+        a4a8ab: '#a4a8ab',
+        two6bba5: '#26bba5',
+        six174ff: '#6174ff'
+      },
+      fontFamily: {
+        baiJamjuree: "'Bai Jamjuree', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
